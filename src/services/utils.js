9c72c64175cbbcd812051911429a8aa3e37201ad
Pablo <pablo_lopesferrao@hotmail.com>
2023-10-18 18:15:48 -0300

    Prueba

--- src/services/utils.js
+++ src/services/utils.js
@@ -5,4 +5,5 @@ const apiUrl = process.env.REACT_APP_API_URL;
 export async function getJsonFromApi(path) {
   const response = await axios.get(`${apiUrl}/${path}`);
   return response.data;
+  "hola mundo"
 }
